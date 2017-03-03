37467dd4a840e5c6af6f6492878c31156c22fa2c
srinivaskodali <nivas.sree.k@gmail.com>
2017-03-03 16:26:21 +0530

    adding linked list

---
+++ LinkedLists/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+
+while(1)  {
+	printf("%d",1);
+	printf("%s\n","Hi Sunil");
+	printf("%s\n","Hi Sunil");
+	printf("%s\n","Hi Sunil");
+	printf("%s\n","Hi Sunil");
+	printf("%s\n","Hi Sunil");
+	printf("%s\n","Hi Sunil");
+	printf("%s\n","Hi Sunil");
+	printf("%s\n","Hi Sunil");
+	printf("%s\n","Hi Sunil");
+	printf("%s\n","Hi Sunil");
+	printf("%s\n","Hi Sunil");
+}
+}
